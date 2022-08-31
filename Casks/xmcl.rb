f052b86e98855f781c59fb5a5578e7cfc6038a95
scientificworld <scientificworld@126.com>
2022-08-31 10:54:08 +0800

    Add Cask xmcl

---
+++ Casks/xmcl.rb
@@ -0,0 +1,10 @@
+cask "xmcl" do
+  version "0.30.5"
+
+  url "https://xmcl-release-ms.azureedge.net/releases/xmcl-#{version}.dmg"
+  name "X Minecraft Launcher"
+  desc "An Open Source Minecraft Launcher with Modern UX."
+  homepage "https://xmcl.app/"
+
+  app "X Minecraft Launcher.app"
+end
