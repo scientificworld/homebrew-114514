a44105e18295e1a5101bc876b82d9167e7ba49ae
scientificworld <scientificworld@126.com>
2022-10-14 20:05:57 +0800

    Fix Formula cryptopp (from madwoolve/homebrew-cryptopp)

--- Formula/cryptopp.rb
+++ Formula/cryptopp.rb
@@ -3,15 +3,15 @@ class Cryptopp < Formula
   homepage "https://cryptopp.com"
   url "https://cryptopp.com/cryptopp870.zip"
   mirror "https://github.com/weidai11/cryptopp/releases/download/CRYPTOPP_8_7_0/cryptopp870.zip"
+  sha256 "d0d3a28fcb5a1f6ed66b3adf57ecfaed234a7e194e42be465c2ba70c744538dd"
   version "8.7.0"
+  license "BSD-3-Clause"
   # Haven't found a way to dynamically generate url by version, hardcode it first.
 
   def install
-    system "make"
-    lib.install "libcryptopp.a"
-    prefix.install "cryptest.exe"
-    include.install Dir["*.h"]
-    include.install Dir["*.cpp"]
+    system "make", "shared", "all", "CXX=#{ENV.cxx}"
+    system "./cryptest.exe", "v"
+    system "make", "install", "PREFIX=#{prefix}"
   end
 
   test do
