d2ef0d228bc0253873b05071ce077bc1c10d94af
scientificworld <scientificworld@126.com>
2022-12-18 01:20:50 +0800

    icalingua-plus-plus: 2.7.7 => 2.8.1

--- Casks/icalingua-plus-plus.rb
+++ Casks/icalingua-plus-plus.rb
@@ -1,6 +1,6 @@
 cask "icalingua-plus-plus" do
-  version "2.7.7"
-  sha256 "c568c15472cdc7e7e6571e73781ccf54a36b61ab22bbf6b8c9b4fadac14072ff"
+  version "2.8.1"
+  sha256 "523f724e8bd4d911052083b8806e138804e022e3ba15281b6845161d76d67a29"
 
   url "https://github.com/Icalingua-plus-plus/Icalingua-plus-plus/releases/download/v#{version}/Icalingua++-#{version}_x64.dmg",
     verified: "https://github.com/Icalingua-plus-plus/Icalingua-plus-plus"
