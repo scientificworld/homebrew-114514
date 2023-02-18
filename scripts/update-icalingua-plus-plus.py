078c525c07b2457881fba7c45d0a2e5957258006
scientificworld <scientificworld@126.com>
2023-02-18 09:58:10 +0800

    [test] auto update via ci

---
+++ scripts/update-icalingua-plus-plus.py
@@ -0,0 +1,14 @@
+import requests, json
+data = requests.get("https://api.github.com/repos/Icalingua-plus-plus/Icalingua-plus-plus/releases/latest").json()
+with open("Casks/icalingua-plus-plus.rb", "w") as file:
+    file.write(f"""cask "icalingua-plus-plus" do
+  version "{data["name"]}"
+
+  url "https://github.com/Icalingua-plus-plus/Icalingua-plus-plus/releases/download/v#{{version}}/Icalingua++-#{{version}}_x64.dmg",
+    verified: "https://github.com/Icalingua-plus-plus/Icalingua-plus-plus"
+  name "Icalingua++"
+  desc "A client for QQ and more."
+  homepage "https://github.com/Icalingua-plus-plus/Icalingua-plus-plus"
+
+  app "Icalingua++.app"
+end""")
