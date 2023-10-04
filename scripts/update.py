c2a63a6dc6e90b96bf0baaaea1b40b362d6a7239
scientificworld <scientificworld@126.com>
2023-10-04 12:35:56 +0800

    [feat] update scripts

--- scripts/update.py
+++ scripts/update.py
@@ -1,10 +1,10 @@
 import requests
-from util import update_util
+from util import update_util, retry_util
 
 # Icalingua++
-data = requests.get("https://api.github.com/repos/Icalingua-plus-plus/Icalingua-plus-plus/releases/latest").json()
+data = retry_util(lambda: requests.get("https://api.github.com/repos/Icalingua-plus-plus/Icalingua-plus-plus/releases/latest").json())
 update_util("Casks/icalingua-plus-plus", ver=data["name"])
 
 # ClashX Meta
-data = requests.get("https://api.github.com/repos/MetaCubeX/ClashX.Meta/releases/latest").json()
+data = retry_util(lambda: requests.get("https://api.github.com/repos/MetaCubeX/ClashX.Meta/releases/latest").json())
 update_util("Casks/clashx-meta", ver=data["name"])
--- scripts/util.py
+++ scripts/util.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import time
+
 def update_util(name, **kwargs):
     with open(Path("templates") / (name + ".rb"), "r") as file:
         content = file.read()
@@ -6,3 +8,11 @@ def update_util(name, **kwargs):
         content = content.replace("#{{" + key + "}}", value)
     with open(name + ".rb", "w") as file:
         file.write(content)
+
+def retry_util(func):
+    for _ in range(3):
+        try:
+            return func()
+        except Exception:
+            time.sleep(5)
+    raise RuntimeError("Reached maximum number of retries")
